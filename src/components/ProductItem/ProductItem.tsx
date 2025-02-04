e9eaae9b1f790e44119c4e13f7d626a5093f8cec
Robun_Gid <robungidquestion@gmail.com>
2025-02-04 17:19:40 +0300

    📦 ProductItem hide add to cart button in ReviewElement

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -16,9 +16,10 @@ import { useSelector } from 'react-redux';
 type ProductItemProps = {
 	product: Product;
 	className?: string;
+	hideAddButton?: boolean;
 };
 
-const ProductItem: FC<ProductItemProps> = ({ product, className }) => {
+const ProductItem: FC<ProductItemProps> = ({ product, className, hideAddButton }) => {
 	const dispatch = useAppDispatch();
 
 	const productCart = useSelector((state: RootState) =>
@@ -97,38 +98,39 @@ const ProductItem: FC<ProductItemProps> = ({ product, className }) => {
 					className='object-cover w-[240px] h-[240px]'
 				/>
 
-				{!count ? (
-					<div
-						className='bg-orange-400 flex w-[100px] absolute justify-center translate-x-2 -translate-y-10 rounded-3xl px-3 py-1 font-bold transition hover:bg-orange-500 hover:scale-105'
-						onClick={handleAddItemToCart}
-					>
-						<FiPlus />
-						<button>Add</button>
-					</div>
-				) : (
-					<div className='bg-orange-400 flex justify-center w-[100px] absolute translate-x-2 -translate-y-10 rounded-3xl px-3 py-1 font-bold transition'>
-						<button
-							className='hover:bg-orange-500 absolute inset-0 w-1/3 hover:rounded-bl-full hover:rounded-tl-full'
+				{!hideAddButton &&
+					(!count ? (
+						<div
+							className='bg-orange-400 flex w-[100px] absolute justify-center translate-x-2 -translate-y-10 rounded-3xl px-3 py-1 font-bold transition hover:bg-orange-500 hover:scale-105'
 							onClick={handleAddItemToCart}
 						>
-							+
-						</button>
-						<div className='text-center' onClick={handleDivClick}>
-							<input
-								type='number'
-								value={count}
-								className='bg-transparent w-1/3 text-center box-border outline-none focus:outline-2 focus:scale-125 focus:outline-amber-700'
-								onChange={handleInputChange}
-							/>
+							<FiPlus />
+							<button>Add</button>
 						</div>
-						<button
-							className='hover:bg-orange-500 absolute top-0 right-0 bottom-0 w-1/3 hover:rounded-tr-full hover:rounded-br-full'
-							onClick={handleRemoveProductFromCart}
-						>
-							–
-						</button>
-					</div>
-				)}
+					) : (
+						<div className='bg-orange-400 flex justify-center w-[100px] absolute translate-x-2 -translate-y-10 rounded-3xl px-3 py-1 font-bold transition'>
+							<button
+								className='hover:bg-orange-500 absolute inset-0 w-1/3 hover:rounded-bl-full hover:rounded-tl-full'
+								onClick={handleAddItemToCart}
+							>
+								+
+							</button>
+							<div className='text-center' onClick={handleDivClick}>
+								<input
+									type='number'
+									value={count}
+									className='bg-transparent w-1/3 text-center box-border outline-none focus:outline-2 focus:scale-125 focus:outline-amber-700'
+									onChange={handleInputChange}
+								/>
+							</div>
+							<button
+								className='hover:bg-orange-500 absolute top-0 right-0 bottom-0 w-1/3 hover:rounded-tr-full hover:rounded-br-full'
+								onClick={handleRemoveProductFromCart}
+							>
+								–
+							</button>
+						</div>
+					))}
 				<div className='flex gap-x-2 items-center'>
 					{productOldPrice ? (
 						<>
