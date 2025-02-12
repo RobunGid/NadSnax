e6ba071bc28063800c6b455a7e5ffefadfec0557
Robun_Gid <robungidquestion@gmail.com>
2025-02-12 20:43:39 +0300

    📦 Item put, delete

--- backend/resources/item.py
+++ backend/resources/item.py
@@ -8,8 +8,44 @@ from sqlalchemy.exc import SQLAlchemyError
 
 blp = Blueprint("items", __name__, description = "Operations on items")
 
-@blp.route('/item')
+@blp.route('/item/<string:item_id>')
 class Item(MethodView):
+	@blp.response(200, ItemSchema)
+	def get(self, item_id):
+		item = ItemModel.query.get_or_404(item_id)
+		return item
+
+	def delete(self, item_id):
+		item = ItemModel.query.get_or_404(item_id)
+		db.session.delete(item)
+		db.session.commit()
+		return {"message": "Item deleted"}
+
+	@blp.response(200, ItemSchema)
+	@blp.arguments(ItemUpdateSchema)
+	def put(self, item_data, item_id):
+		item = ItemModel.query.get(item_id)
+  
+		if item:
+			item.label = item_data["label"]
+			item.page_link = item_data["page_link"]
+			item.description = item_data["description"]
+			item.image_url = item_data["image_url"]
+			item.price = item_data["price"]
+			item.old_price = item_data["old_price"]
+			item.is_bestseller = item_data["is_bestseller"]
+			item.category_id = item_data["category_id"]
+			item.type_id = item_data["type_id"]
+		else:
+			item = ItemModel(**item_data, id = item_id)
+                    
+		db.session.add(item)
+		db.session.commit()
+        
+		return item
+
+@blp.route('/item')
+class Items(MethodView):
     @blp.response(200, ItemSchema(many = True))
     def get(self):
         return ItemModel.query.all()
