b5976c79741cc5346388d86758491671a8981b38
Robun_Gid <robungidorig@gmail.com>
2025-03-06 21:04:12 +0300

    ⚫ Aded shadow to ProductDetailsImages

--- frontend/src/components/ProductDetails/ProductDetailsImages.tsx
+++ frontend/src/components/ProductDetails/ProductDetailsImages.tsx
@@ -32,7 +32,7 @@ export const ProductDetailsImages: FC<ProductDetailsImagesProps> = ({
 					<label
 						htmlFor={`img-tab-${index}`}
 						style={{ backgroundImage: `url(${image.url})` }}
-						className='block cursor-pointer bg-no-repeat bg-cover position bg-center rounded-md transition-transform'
+						className='block cursor-pointer bg-no-repeat bg-cover position bg-center rounded-md transition-transform shadow-sm shadow-black'
 					></label>
 					<div className='opacity-0 transition-opacity md:col-start-2 md:col-end-2 md:row-start-1 md:row-end-5 col-start-1 col-end-5 row-start-1 row-end-1'>
 						<img src={image.url} />
