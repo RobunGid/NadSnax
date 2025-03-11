62320a038f630f708ce8d050fc128a4092a4bc9d
Robun_Gid <robungidorig@gmail.com>
2025-03-11 23:53:56 +0300

    🌞 Improved theme switcher visibility

--- frontend/src/components/layout/ThemeSwitcher.tsx
+++ frontend/src/components/layout/ThemeSwitcher.tsx
@@ -1,9 +1,18 @@
+import clsx from 'clsx';
 import { FC } from 'react';
 
-export const ThemeSwitcher: FC = () => {
+interface ThemeSwitcherProps {
+	className: string;
+}
+
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
 	return (
-		<div>
-			<input type='checkbox' id='theme-swicher-input' />
+		<div className='flex items-center'>
+			<label className={clsx('relative inline-block w-14 h-8', className)}>
+				<input type='checkbox' className='opacity-0 w-0 h-0 peer' />
+				<span className='peer-checked:bg-amber-500 peer-focus:shadow-sm peer-checked:before:translate-x-6 rounded-3xl before:rounded-2xl absolute cursor-pointer inset-0 bg-gray-500 transition before:absolute before:h-6 before:w-6 before:left-1 before:bottom-1 before:bg-white before:transition' />
+			</label>
+			<div className='dark:text-white text-xs'>Change App Theme</div>
 		</div>
 	);
 };
--- frontend/src/components/layout/UserProfile.tsx
+++ frontend/src/components/layout/UserProfile.tsx
@@ -32,7 +32,7 @@ const UserProfile: FC<UserProfileProps> = ({ className }) => {
 			</div>
 
 			<div className='overflow-hidden transition-all duration-200 max-h-0 peer-has-[:checked]:max-h-[300px] absolute right-2 top-16 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600'>
-				<ThemeSwitcher />
+				<ThemeSwitcher className='m-2' />
 				<div className='px-4 py-3 text-sm text-gray-900 dark:text-white'>
 					<div>{user.name}</div>
 					<div className='font-medium truncate'>{user.email}</div>
