5df6d89c847e4789469117baacc58176d87e4442
Robun_Gid <robungidorig@gmail.com>
2025-03-25 02:10:55 +0300

    🔧 ProductDetailsQuantityChooser refactoring (partly), cartSelectors type fixed

--- frontend/src/store/cartSelectors.ts
+++ frontend/src/store/cartSelectors.ts
@@ -4,6 +4,6 @@ export const selectAllItems = (state: RootState) => {
 	return state.cart.productList;
 };
 
-export const selectItemById = (state: RootState, id: string) => {
+export const selectItemById = (state: RootState, id?: string) => {
 	return state.cart.productList.find((cartItem) => cartItem.item.id === id);
 };
