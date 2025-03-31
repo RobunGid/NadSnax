d625a723503d858d1c6f7cb88081daede588a461
Robun_Gid <robungidorig@gmail.com>
2025-03-31 23:59:43 +0300

    🔧 Fix skeleton loader animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,17 @@ export default {
 				'gray-650': 'oklch(0.4095 0.032 258.268)',
 				'gray-250': 'oklch(0.9 0.008 261.435)',
 			},
+			animation: {
+				fade: 'fadeIn 0.5s ease-in-out',
+			},
+
+			keyframes: () => ({
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'30%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			}),
 		},
 	},
 };
