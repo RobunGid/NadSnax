e43600f9105341ede583fd3fe413a518d49cfaa2
Robun_Gid <robungidorig@gmail.com>
2025-04-08 23:37:16 +0300

    ♻️ UISidebarHeader fixes refactoring

--- frontend/src/components/Sidebar/UI/UISidebarHeader.tsx
+++ frontend/src/components/Sidebar/UI/UISidebarHeader.tsx
@@ -1,23 +1,22 @@
+import { Link } from 'react-router';
 import { UISiteLogo } from '../../UI/UISiteLogo';
-import { SidebarLink } from '../SidebarLink';
 import { MouseEventHandler } from 'react';
 
 interface UISidebarHeaderProps {
-	onClick?: MouseEventHandler<HTMLDivElement>;
+	onClick?: MouseEventHandler<HTMLAnchorElement>;
 }
 
 export const UISidebarHeader = ({ onClick }: UISidebarHeaderProps) => {
 	return (
-		<div
+		<Link
+			to='/home'
 			onClick={onClick}
 			className='flex items-center justify-start rtl:justify-end'
 		>
-			<SidebarLink to='/home'>
-				<UISiteLogo />
-				<span className='self-center text-xl font-semibold sm:text-2xl whitespace-nowrap'>
-					NadSnax
-				</span>
-			</SidebarLink>
-		</div>
+			<UISiteLogo />
+			<span className='self-center text-xl font-semibold sm:text-2xl whitespace-nowrap'>
+				NadSnax
+			</span>
+		</Link>
 	);
 };
