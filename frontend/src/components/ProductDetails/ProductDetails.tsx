acdd138cd1b3579c96020d5b22ff7e7294f5ae0c
RobunGid <robungidorig@gmail.com>
2025-04-20 23:50:40 +0300

    🛍️ UIProductDetails component added

--- frontend/src/components/ProductDetails/ProductDetails.tsx
+++ frontend/src/components/ProductDetails/ProductDetails.tsx
@@ -2,6 +2,7 @@ import { Item } from '../../types';
 import { ProductDetailsGallery } from './ProductDetailsGallery';
 import { ProductDetailsInfo } from './ProductDetailsInfo';
 import { ProductDetailsSimillarItems } from './ProductDetailsSimillarItems';
+import { UIProductDetails } from './UI/UIProductDetails';
 
 interface ProductDetailsProps {
 	item: Item;
@@ -10,10 +11,10 @@ interface ProductDetailsProps {
 
 export const ProductDetails = ({ item, itemList }: ProductDetailsProps) => {
 	return (
-		<div className='p-3 flex flex-row gap-10 pt-16 flex-wrap justify-center md:justify-start dark:text-gray-200'>
+		<UIProductDetails>
 			<ProductDetailsGallery className='md:ml-8' images={item.images} />
 			<ProductDetailsInfo item={item} />
 			<ProductDetailsSimillarItems item={item} itemList={itemList} />
-		</div>
+		</UIProductDetails>
 	);
 };
---
+++ frontend/src/components/ProductDetails/UI/UIProductDetails.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+interface UIProductDetailsProps {
+	children: ReactNode;
+}
+
+export const UIProductDetails = ({ children }: UIProductDetailsProps) => {
+	return (
+		<div className='p-3 flex flex-row gap-10 pt-16 flex-wrap justify-center md:justify-start dark:text-gray-200'>
+			{children}
+		</div>
+	);
+};
