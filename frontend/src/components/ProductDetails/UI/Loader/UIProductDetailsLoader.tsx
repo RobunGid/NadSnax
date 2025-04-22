f3a5233022a2acf91a5efd84a09b23d88f985f97
RobunGid <robungidorig@gmail.com>
2025-04-22 20:32:07 +0300

    🌀 UIProductDetailsLoader almost almost done

--- frontend/src/components/ProductDetails/UI/Loader/UIProductDetailsLoader.tsx
+++ frontend/src/components/ProductDetails/UI/Loader/UIProductDetailsLoader.tsx
@@ -1,11 +1,13 @@
 import { UIProductDetailsLoaderGallery } from './UIProductDetailsLoaderGallery';
 import { UIProductDetailsLoaderInfo } from './UIProductDetailsLoaderInfo';
+import { UIProductDetailsLoaderSimillarItems } from './UIProductDetailsLoaderSimillarItems';
 
 export const UIproductDetailsLoader = () => {
 	return (
 		<div className='p-3 flex flex-row gap-10 pt-16 flex-wrap justify-center md:justify-start dark:text-gray-200'>
 			<UIProductDetailsLoaderGallery />
 			<UIProductDetailsLoaderInfo />
+			<UIProductDetailsLoaderSimillarItems />
 		</div>
 	);
 };
