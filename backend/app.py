289d31891ab1aa8763f75244f061242dcd0a7367
RobunGid <robungidorig@gmail.com>
2025-04-23 20:37:32 +0300

    🐘 Moved from sqlite to postgre

--- backend/app.py
+++ backend/app.py
@@ -29,8 +29,14 @@ def create_app(db_url = None):
 	app.config["OPENAPI_URL_PREFIX"] = "/"
 	app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 	app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-	app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+ 
+	db_url = os.getenv("DATABASE_URL")
+	if db_url is not None:
+		app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+			"postgres://", "postgresql://")
+	else:
+		app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
  
 	db.init_app(app)
 
