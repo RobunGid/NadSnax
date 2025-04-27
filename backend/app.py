c67c667e97dc8317332b15aac532c7881cda7875
RobunGid <robungidorig@gmail.com>
2025-04-27 23:23:54 +0300

    🚪 Logout added

--- backend/app.py
+++ backend/app.py
@@ -18,58 +18,64 @@ from resources.image import blp as ImageBlueprint
 
 from datetime import timedelta
 
+from blocklist import BLOCKLIST
+
 def create_app(db_url = None):
-	app = Flask(__name__)
+    app = Flask(__name__)
  
-	env = os.getenv('APP_ENV', 'prod')
-	env_file = '.env.local' if env == 'dev' else '.env'
-	load_dotenv(env_file, override=True)	
+    env = os.getenv('APP_ENV', 'prod')
+    env_file = '.env.local' if env == 'dev' else '.env'
+    load_dotenv(env_file, override=True)	
  
-	migrate = Migrate(app, db)
-	jwt = JWTManager(app)	
+    migrate = Migrate(app, db)
+    jwt = JWTManager(app)	
+ 
+    @jwt.token_in_blocklist_loader
+    def check_if_token_revoked(jwt_header, jwt_payload):
+        return jwt_payload['jti'] in BLOCKLIST
  
  
-	CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
-	app.config["PROPOGATE_EXCEPTIONS"] = True
-	app.config["API_TITLE"] = "NadSnax REST API"
-	app.config["API_VERSION"] = "v1"
-	app.config["OPENAPI_VERSION"] = "3.0.3"
-	app.config["OPENAPI_URL_PREFIX"] = "/"
-	app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-	app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-	app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
-	app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]
-	app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=5)
-	app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
-	app.config["JWT_COOKIE_SECURE"] = True
-	app.config["JWT_COOKIE_SAMESITE"] = "Strict"
-	app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+    app.config["PROPOGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "NadSnax REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET')
+    app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=5)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+    app.config["JWT_COOKIE_SECURE"] = True
+    app.config["JWT_COOKIE_SAMESITE"] = "Strict"
+    app.config["JWT_COOKIE_CSRF_PROTECT"] = False
  
 
-	db_url = os.getenv("DATABASE_URL", 'sqlite:////tmp/test.db')
+    db_url = os.getenv("DATABASE_URL", 'sqlite:////tmp/test.db')
 
-	if db_url is not None:
-		app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-	else:
-		app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    if db_url is not None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
  
-	db.init_app(app)
+    db.init_app(app)
 
-	api = Api(app)
+    api = Api(app)
  
-	with app.app_context():
-		db.create_all()
+    with app.app_context():
+        db.create_all()
 
-	api.register_blueprint(ImageBlueprint)
-	api.register_blueprint(UserBlueprint)
-	api.register_blueprint(ReviewBlueprint)
-	api.register_blueprint(CategoryBlueprint)
-	api.register_blueprint(TypeBlueprint)
-	api.register_blueprint(ItemBlueprint)
-	api.register_blueprint(ItemDetailsBlueprint)
+    api.register_blueprint(ImageBlueprint)
+    api.register_blueprint(UserBlueprint)
+    api.register_blueprint(ReviewBlueprint)
+    api.register_blueprint(CategoryBlueprint)
+    api.register_blueprint(TypeBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(ItemDetailsBlueprint)
  
-	return app
+    return app
 
 app = create_app()
--- backend/resources/user.py
+++ backend/resources/user.py
@@ -5,11 +5,11 @@ from schemas import UserSchema, UserUpdateSchema, PlainUserSchema
 from db import db
 from sqlalchemy.exc import SQLAlchemyError
 import uuid
-from flask_jwt_extended import create_access_token, set_refresh_cookies, create_refresh_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, set_refresh_cookies, create_refresh_token, jwt_required, get_jwt_identity, get_jwt
 from flask import request, jsonify
 from passlib.hash import pbkdf2_sha512
 from utils import role_required
-
+from blocklist import BLOCKLIST	
 
 blp = Blueprint("users", __name__, description = "Operations on users")
 
@@ -25,106 +25,117 @@ class MyUser(MethodView):
 
 @blp.route('/user/<string:user_id>')
 class User(MethodView):
-	@blp.response(200, UserSchema)
-	@jwt_required()
-	@role_required(['admin', 'moderator'])
-	def get(self, user_id):
-		user = UserModel.query.get_or_404(user_id)
-		return user
-	
-	@role_required(['admin', 'moderator'])
-	def delete(self, user_id):
-		user = UserModel.query.get_or_404(user_id)
-		db.session.delete(user)
-		db.session.commit()
-		return {"message": "User deleted", "code": 202}
+    @blp.response(200, UserSchema)
+    @jwt_required()
+    @role_required(['admin', 'moderator'])
+    def get(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        return user
+    
+    @role_required(['admin', 'moderator'])
+    def delete(self, user_id):
+        user = UserModel.query.get_or_404(user_id)
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": "User deleted", "code": 202}
 
-	@role_required(['admin', 'moderator'])
-	@blp.response(200, UserSchema)        
-	@blp.arguments(UserUpdateSchema)
-	def put(self, user_data, user_id):
-		user = UserModel.query.get(user_id)
-		
-		if user:
-			user.username = user_data["username"]
-			user.avatar_url = user_data["avatar_url"]
-			user.fisrt_name = user_data["first_name"]
-			user.last_name = user_data["last_name"]
-			user.role = user_data["role"]
-		else:
-			user = UserModel(id = user_id, **user_data)
-			
-		db.session.add(user)
-		db.session.commit()
-		
-		return user
+    @role_required(['admin', 'moderator'])
+    @blp.response(200, UserSchema)        
+    @blp.arguments(UserUpdateSchema)
+    def put(self, user_data, user_id):
+        user = UserModel.query.get(user_id)
+        
+        if user:
+            user.username = user_data["username"]
+            user.avatar_url = user_data["avatar_url"]
+            user.fisrt_name = user_data["first_name"]
+            user.last_name = user_data["last_name"]
+            user.role = user_data["role"]
+        else:
+            user = UserModel(id = user_id, **user_data)
+            
+        db.session.add(user)
+        db.session.commit()
+        
+        return user
 
 
 @blp.route('/user')
 class Users(MethodView):
-	@blp.response(200, UserSchema(many = True))
-	@jwt_required()
-	@role_required(['moderator', 'admin'])
-	def get(self):
-		username_filter = request.args.get("username")
-		first_name_filter = request.args.get("first_name")
-		last_name_filter = request.args.get("last_name")
-		avatar_url_filter = request.args.get("avatar_url")
+    @blp.response(200, UserSchema(many = True))
+    @jwt_required()
+    @role_required(['moderator', 'admin'])
+    def get(self):
+        username_filter = request.args.get("username")
+        first_name_filter = request.args.get("first_name")
+        last_name_filter = request.args.get("last_name")
+        avatar_url_filter = request.args.get("avatar_url")
   
-		query = UserModel.query
+        query = UserModel.query
   
-		query = query.filter(UserModel.username==username_filter) if username_filter else query
-		query = query.filter(UserModel.first_name==last_name_filter) if first_name_filter else query
-		query = query.filter(UserModel.last_name==first_name_filter) if last_name_filter else query
-		query = query.filter(UserModel.avatar_url==avatar_url_filter) if avatar_url_filter else query
+        query = query.filter(UserModel.username==username_filter) if username_filter else query
+        query = query.filter(UserModel.first_name==last_name_filter) if first_name_filter else query
+        query = query.filter(UserModel.last_name==first_name_filter) if last_name_filter else query
+        query = query.filter(UserModel.avatar_url==avatar_url_filter) if avatar_url_filter else query
   
-		return query.all()
-	
-	   
+        return query.all()
+    
+       
 @blp.route('/register')
 class UserRegister(MethodView):
-	@blp.arguments(UserSchema)
-	@blp.response(201, UserSchema)    
-	def post(self, user_data):
-		user = UserModel(
-	  id = str(uuid.uuid4()),
-	  username=user_data["username"], 
-	  password=pbkdf2_sha512.hash(user_data["password"]), 
-	  avatar_url=user_data["avatar_url"],
-	  first_name=user_data["first_name"],
-	  last_name=user_data["last_name"],
-	  role=user_data["role"],
-	  )
-		try:
-			db.session.add(user)
-			db.session.commit()
-		except SQLAlchemyError:
-			abort(500, message = "An error occured while inserting the user")
-	
-		return user
+    @blp.arguments(UserSchema)
+    @blp.response(201, UserSchema)    
+    def post(self, user_data):
+        user = UserModel(
+      id = str(uuid.uuid4()),
+      username=user_data["username"], 
+      password=pbkdf2_sha512.hash(user_data["password"]), 
+      avatar_url=user_data["avatar_url"],
+      first_name=user_data["first_name"],
+      last_name=user_data["last_name"],
+      role=user_data["role"],
+      )
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message = "An error occured while inserting the user")
+    
+        return user
 
 
 @blp.route('/login')
 class UserLogin(MethodView):
-	@blp.arguments(PlainUserSchema)
-	def post(self, user_data):
-		user = UserModel.query.filter(
-			UserModel.username == user_data["username"]
-		).first()
-		
-		if user and pbkdf2_sha512.verify(user_data["password"], user.password):
-			access_token = create_access_token(identity = str(user.id), fresh = True)
-			refresh_token = create_refresh_token(identity = str(user.id))
-			response = jsonify({"access_token": access_token})
-			set_refresh_cookies(response, refresh_token)
-			return response
-	
-		abort(401, description = "Invalid credentials")
-		
+    @blp.arguments(PlainUserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+        
+        if user and pbkdf2_sha512.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity = str(user.id), fresh = True)
+            refresh_token = create_refresh_token(identity = str(user.id))
+            response = jsonify({"access_token": access_token})
+            set_refresh_cookies(response, refresh_token)
+            return response
+    
+        abort(401, description = "Invalid credentials")
+        
 @blp.route('/refresh')
 class TokenRefresh(MethodView):
-	@jwt_required(refresh=True)
-	def post(self):
-		identity = get_jwt_identity()
-		access_token = create_access_token(identity=identity)
-		return jsonify(access_token=access_token)
+    @jwt_required(refresh=True)
+    def post(self):
+        identity = get_jwt_identity()
+        access_token = create_access_token(identity=identity)
+        return jsonify(access_token=access_token)
+
+@blp.route('/logout')
+class Logout(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        response = jsonify(msg="Logout successful")
+        response.set_cookie('refresh_token_cookie', '', expires=0, path='/refresh', httponly=True)
+        response.set_cookie('access_token_cookie', '', expires=0, path='/', httponly=True)
+        return response
