8a355fd9a694ad7d0d099afebc82638fbc20a45a
RobunGid <robungidorig@gmail.com>
2025-04-30 23:58:44 +0300

    🔧 Fixes in index.css, tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,41 +3,6 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	darkMode: 'class',
 	theme: {
-		extend: {
-			fontFamily: {
-				opensans: ['Open Sans', 'sans-serif'],
-			},
-			colors: {
-				'gray-650': 'oklch(0.4095 0.032 258.268)',
-				'gray-250': 'oklch(0.9 0.008 261.435)',
-				'gray-825': 'oklch(0.25125 0.03425 261.462)',
-				'gray-750': 'oklch(0.3255 0.0355 258.159)',
-			},
-			animation: {
-				skeleton:
-					'skeletonFadeIn 0.4s ease-in-out 0.25s 1 normal forwards, skeletonPulse 2s cubic-bezier(0.4, 0, 0.6, 1) 0.2s infinite;',
-				fadeIn: 'fadeIn 0.15s ease-in-out 0s forwards;',
-			},
-
-			keyframes: () => ({
-				skeletonFadeIn: {
-					'0%': { opacity: 0 },
-					'100%': { opacity: 1 },
-				},
-				skeletonPulse: {
-					'50%': {
-						opacity: 0.65,
-					},
-				},
-				fadeIn: {
-					'0%': {
-						opacity: 0,
-					},
-					'100%': {
-						opacity: 1,
-					},
-				},
-			}),
-		},
+		extend: {},
 	},
 };
