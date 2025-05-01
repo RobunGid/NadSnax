cb031b6b6e4db1083c338fc4d69f9c729b2dec6b
RobunGid <robungidorig@gmail.com>
2025-05-01 22:31:30 +0300

    🔧 Divides and borders fixes in UINavbar

--- frontend/src/components/Navbar/UI/UINavbarNavigation.tsx
+++ frontend/src/components/Navbar/UI/UINavbarNavigation.tsx
@@ -6,7 +6,7 @@ interface UINavbarNavigation {
 
 export const UINavbarNavigation = ({ children }: UINavbarNavigation) => {
 	return (
-		<ul className='flex-row hidden md:flex md:gap-3 grow dark:text-gray-200 divide-x-[1px] divide-gray-500'>
+		<ul className='flex-row hidden md:flex md:gap-3 grow dark:text-gray-200 divide-x-[1px] divide-gray-300'>
 			{children}
 		</ul>
 	);
