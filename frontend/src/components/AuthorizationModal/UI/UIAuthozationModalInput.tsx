7b22a6f8e779597418ffb3cd1da294a227ed97f2
RobunGid <robungidorig@gmail.com>
2025-05-01 22:36:03 +0300

    🎨 UIAuthorizationModalInput fixes

--- frontend/src/components/AuthorizationModal/UI/UIAuthozationModalInput.tsx
+++ frontend/src/components/AuthorizationModal/UI/UIAuthozationModalInput.tsx
@@ -9,13 +9,13 @@ type UIAuthozationModalInputProps = {
 export const UIAuthozationModalInput = (props: UIAuthozationModalInputProps) => {
 	const { errorMessage, error, placeholder } = props;
 	return (
-		<label className='relative flex flex-col-reverse'>
+		<label className='relative flex flex-col-reverse '>
 			<input
 				className={`
-				mb-2.5 w-72 h-10 font-normal pt-5 px-4 pb-5 rounded-lg border-none relative placeholder-transparent text-black outline-none
-				focus:shadow-lg focus:shadow-black
+				mb-2.5 w-72 h-10 font-normal pt-5 px-4 pb-5 rounded-lg border-none relative placeholder-transparent  outline-none
+				focus:shadow-lg focus:shadow-gray-900
 				invalid:outline-red-800 invalid:outline-4
-				peer
+				peer shadow-lg dark:text-gray-300 text-gray-900
 				`}
 				{...props}
 			/>
@@ -24,9 +24,9 @@ export const UIAuthozationModalInput = (props: UIAuthozationModalInputProps) => 
 			</p>
 			<p
 				className={`
-					opacity-0 absolute bottom-[18px] left-4 font-normal transition-all text-black pointer-events-none
-					peer-focus:bottom-8 peer-focus:text-sm peer-focus:text-gray-500
-					peer-placeholder-shown:opacity-100 
+					opacity-0 absolute bottom-[18px] left-4 font-normal transition-all  pointer-events-none
+					peer-focus:bottom-8 peer-focus:text-xs peer-focus:text-gray-700
+					peer-placeholder-shown:opacity-100 text-gray-600
 					`}
 			>
 				{placeholder}
