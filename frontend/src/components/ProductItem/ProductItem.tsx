c220bb47f9eb4a82df1f2e703f94e3f75ae16e9e
RobunGid <robungidorig@gmail.com>
2025-05-05 08:09:18 +0300

    ♻️ ProductItem fixes in div/Link

--- frontend/src/components/ProductItem/ProductItem.tsx
+++ frontend/src/components/ProductItem/ProductItem.tsx
@@ -34,43 +34,39 @@ export const ProductItem = ({ item, className }: ProductItemProps) => {
 	const oldPrice = item.oldPrice ? formatPrice(item.oldPrice) : '';
 
 	return (
-		<div className='hover:scale-[102.5%] transition-transform animate-fadeIn opacity-0'>
-			<Link
-				to={`/products/page${item.pageLink}`}
-				className={clsx(
-					'shadow-xl dark:shadow-gray-950 h-[360px] p-2 w-60',
-					className,
-					styles['product-item']
-				)}
-			>
-				<div>
-					<UIProductItemImage
-						imageURL={imageURL}
-						isBestseller={item.isBestseller}
-						isSecretbox={!!item.isSecretbox}
-						label={item.label}
-					/>
+		<Link
+			to={`/products/page${item.pageLink}`}
+			className={clsx(
+				'shadow-xl h-[360px] p-2 w-60 block hover:scale-[102.5%] transition-transform animate-fadeIn opacity-0',
+				className,
+				styles['product-item']
+			)}
+		>
+			<UIProductItemImage
+				imageURL={imageURL}
+				isBestseller={item.isBestseller}
+				isSecretbox={!!item.isSecretbox}
+				label={item.label}
+			/>
 
-					<ProductItemQuantityChooser
-						cartItem={cartItem}
-						onAdd={handleAddItemToCart}
-						onDelete={handleRemoveProductFromCart}
-						onInputChange={handleInputChange}
-						onRemove={handleRemoveProductFromCart}
-					/>
+			<ProductItemQuantityChooser
+				cartItem={cartItem}
+				onAdd={handleAddItemToCart}
+				onDelete={handleRemoveProductFromCart}
+				onInputChange={handleInputChange}
+				onRemove={handleRemoveProductFromCart}
+			/>
 
-					<UIProductItemPrice price={price} oldPrice={oldPrice} />
-					<UIProductItemLabel
-						description={item.description}
-						label={item.label}
-						isBestseller={item.isBestseller}
-					/>
-					<UIProductItemRating
-						ratingCount={item.ratingCount}
-						averageRating={item.averageRating}
-					/>
-				</div>
-			</Link>
-		</div>
+			<UIProductItemPrice price={price} oldPrice={oldPrice} />
+			<UIProductItemLabel
+				description={item.description}
+				label={item.label}
+				isBestseller={item.isBestseller}
+			/>
+			<UIProductItemRating
+				ratingCount={item.ratingCount}
+				averageRating={item.averageRating}
+			/>
+		</Link>
 	);
 };
