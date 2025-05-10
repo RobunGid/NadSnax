f3e3fb31f00570975fba8aad4dd34a7a28c4d244
RobunGid <robungidorig@gmail.com>
2025-05-10 23:46:55 +0300

    📝 ReviewForm improvements

--- frontend/src/components/ProductDetails/ProductDetails.tsx
+++ frontend/src/components/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,7 @@
 import { Item } from '../../types';
 import { ProductDetailsGallery } from './ProductDetailsGallery';
 import { ProductDetailsInfo } from './ProductDetailsInfo';
+import { ProductDetailsReviewForm } from './ProductDetailsReviewForm';
 import { ProductDetailsReviews } from './ProductDetailsReviews';
 import { ProductDetailsSimillarItems } from './ProductDetailsSimillarItems';
 import { UIProductDetails } from './UI/UIProductDetails';
@@ -15,6 +16,7 @@ export const ProductDetails = ({ item, itemList }: ProductDetailsProps) => {
 		<UIProductDetails>
 			<ProductDetailsGallery images={item.images} />
 			<ProductDetailsInfo item={item} />
+			<ProductDetailsReviewForm />
 			<ProductDetailsReviews reviews={item.reviews} />
 			<ProductDetailsSimillarItems item={item} itemList={itemList} />
 		</UIProductDetails>
--- frontend/src/components/ProductDetails/ProductDetailsReviewForm.tsx
+++ frontend/src/components/ProductDetails/ProductDetailsReviewForm.tsx
@@ -1,3 +1,82 @@
+import { GoStarFill } from 'react-icons/go';
+import { UIButton } from '../UI/UIButton';
+
 export const ProductDetailsReviewForm = () => {
-	return <form></form>;
+	return (
+		<form className='shadow-lg dark:shadow-gray-600 p-8 gap-y-4 flex flex-col items-center'>
+			<div>Rate this item</div>
+			<div className='flex flex-row-reverse justify-end items-center'>
+				<input
+					id='hs-ratings-readonly-1'
+					type='radio'
+					className='peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0'
+					name='hs-ratings-readonly'
+					value='1'
+				/>
+				<label
+					htmlFor='hs-ratings-readonly-1'
+					className='peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-neutral-600'
+				>
+					<GoStarFill />
+				</label>
+				<input
+					id='hs-ratings-readonly-2'
+					type='radio'
+					className='peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0'
+					name='hs-ratings-readonly'
+					value='2'
+				/>
+				<label
+					htmlFor='hs-ratings-readonly-2'
+					className='peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-neutral-600'
+				>
+					<GoStarFill />
+				</label>
+				<input
+					id='hs-ratings-readonly-3'
+					type='radio'
+					className='peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0'
+					name='hs-ratings-readonly'
+					value='3'
+				/>
+				<label
+					htmlFor='hs-ratings-readonly-3'
+					className='peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-neutral-600'
+				>
+					<GoStarFill />
+				</label>
+				<input
+					id='hs-ratings-readonly-4'
+					type='radio'
+					className='peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0'
+					name='hs-ratings-readonly'
+					value='4'
+				/>
+				<label
+					htmlFor='hs-ratings-readonly-4'
+					className='peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-neutral-600'
+				>
+					<GoStarFill />
+				</label>
+				<input
+					id='hs-ratings-readonly-5'
+					type='radio'
+					className='peer -ms-5 size-5 bg-transparent border-0 text-transparent cursor-pointer appearance-none checked:bg-none focus:bg-none focus:ring-0 focus:ring-offset-0'
+					name='hs-ratings-readonly'
+					value='5'
+				/>
+				<label
+					htmlFor='hs-ratings-readonly-5'
+					className='peer-checked:text-yellow-400 text-gray-300 pointer-events-none dark:peer-checked:text-yellow-600 dark:text-neutral-600'
+				>
+					<GoStarFill />
+				</label>
+			</div>
+			<textarea
+				className='resize-none shadow-lg outline-none p-4'
+				placeholder='Tell us about your opinion!'
+			></textarea>
+			<UIButton className='w-28'>Send</UIButton>
+		</form>
+	);
 };
