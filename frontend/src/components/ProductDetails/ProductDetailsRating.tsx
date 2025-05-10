40c1bee70b38a900691dd53ce50199ed15e7d281
RobunGid <robungidorig@gmail.com>
2025-05-10 15:51:10 +0300

    🌟 ProductDetailsRating fixes

--- frontend/src/components/ProductDetails/ProductDetailsRating.tsx
+++ frontend/src/components/ProductDetails/ProductDetailsRating.tsx
@@ -10,9 +10,13 @@ export const ProductDetailsRating = ({
 	ratingCount,
 }: ProductDetailsRatingProps) => {
 	return (
-		<div className='flex'>
-			<RatingElement rating={averageRating} className='flex text-yellow-500' />
-			<div>{ratingCount}</div>
+		<div className='flex justify-start items-center gap-x-1'>
+			<RatingElement rating={averageRating} className='flex text-yellow-400' />
+			<span className='text-gray-400'>{averageRating || 0}</span>
+			<span className='w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400'></span>
+			<span className='text-gray-500'>
+				{ratingCount} {ratingCount ? 'reviews' : 'No reviews'}
+			</span>
 		</div>
 	);
 };
