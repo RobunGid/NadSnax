b616dad07266a6c7e40cf07cc0d902feab086f90
RobunGid <robungidorig@gmail.com>
2025-05-10 15:46:45 +0300

    🌟 UIProductItemRating improvements

--- frontend/src/components/ProductItem/UI/UIProductItemRating.tsx
+++ frontend/src/components/ProductItem/UI/UIProductItemRating.tsx
@@ -10,13 +10,17 @@ export const UIProductItemRating = ({
 	averageRating,
 }: UIProductItemRatingProps) => {
 	return (
-		<div className='flex justify-start'>
+		<div className='flex justify-start items-center gap-x-1'>
 			<RatingElement
 				rating={averageRating}
 				size='16'
 				className='flex text-yellow-400'
 			/>
-			<span className='text-gray-500 text-[0.75rem]'>{ratingCount || 0}</span>
+			<span className='text-gray-400 text-[0.75rem]'>{averageRating || 0}</span>
+			<span className='w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400'></span>
+			<span className='text-gray-500 text-[0.75rem]'>
+				{ratingCount} {ratingCount ? 'reviews' : 'No reviews'}
+			</span>
 		</div>
 	);
 };
