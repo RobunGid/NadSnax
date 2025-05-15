507c72cdc243f90fd87b9b38425fbb05571877ce
RobunGid <robungidorig@gmail.com>
2025-05-15 23:53:23 +0300

    🔐 PrivateRoutes toggleLoginModalVisibility added

--- frontend/src/components/Layout/PrivateRoutes.tsx
+++ frontend/src/components/Layout/PrivateRoutes.tsx
@@ -1,11 +1,22 @@
 import { Navigate, Outlet } from 'react-router';
 import { useStateSelector } from '../../store';
+import { LoginModalContext } from '../../context/LoginModalContext';
+import { useContext, useEffect } from 'react';
 
 export const PrivateRoutes = () => {
 	const accessToken = useStateSelector((state) => state.auth.accessToken);
 	const accessTokenStatus = useStateSelector((state) => state.auth.status);
+	useEffect(() => {
+		if (accessTokenStatus == 'error') {
+			toggleLoginModalVisibility();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [accessTokenStatus]);
+	const { toggleLoginModalVisibility } = useContext(LoginModalContext);
 	if (accessToken || accessTokenStatus == 'success') {
 		return <Outlet />;
 	}
-	if (accessTokenStatus == 'error') return <Navigate to='/home' replace />;
+	if (accessTokenStatus == 'error') {
+		return <Navigate to='/home' replace />;
+	}
 };
