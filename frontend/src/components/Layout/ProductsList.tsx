ef216fcad6bf9cc250921344b12d3949a8cfb07e
RobunGid <robungidorig@gmail.com>
2025-05-19 22:34:24 +0300

    ➡️  Remove params parameter from ProductsList

--- frontend/src/components/Layout/ProductsList.tsx
+++ frontend/src/components/Layout/ProductsList.tsx
@@ -1,15 +1,13 @@
-import { fetchItemsParams } from '../../store';
 import { Item } from '../../types';
 import { ProductItem } from '../ProductItem/ProductItem';
 
 interface ProductsListProps {
 	items: Item[];
 	className?: string;
-	params: fetchItemsParams;
 }
 
-export const ProductsList = ({ items, className, params }: ProductsListProps) => {
+export const ProductsList = ({ items, className }: ProductsListProps) => {
 	return items.map((item) => (
-		<ProductItem className={className} key={item.id} item={item} params={params} />
+		<ProductItem className={className} key={item.id} item={item} />
 	));
 };
