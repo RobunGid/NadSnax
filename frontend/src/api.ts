ed6d12547c23d513c75c2ec4722104def61884a3
RobunGid <robungidorig@gmail.com>
2025-05-21 21:54:07 +0300

    🗑️ Delete unnecessary function in api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,10 +1,6 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { Item } from './types';
+import axios from 'axios';
 
 export const Axios = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
 	withCredentials: true,
 });
-
-export const fetchSimillarItems = async (item: Item, parameters?: AxiosRequestConfig) =>
-	Axios.get<Item[]>('/item', { params: { simillar_id: item.id, ...parameters } });
