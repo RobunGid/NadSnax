44bec40abbdde80799807a5d5725f8db039d0b5e
RobunGid <robungidorig@gmail.com>
2025-06-16 20:53:16 +0300

    🖱️ ProductDetailsGallery improving

--- frontend/src/components/ProductDetails/UI/UIProductDetailsGallery.tsx
+++ frontend/src/components/ProductDetails/UI/UIProductDetailsGallery.tsx
@@ -1,20 +1,26 @@
 import clsx from 'clsx';
 import classes from './UIProductDetailsGallery.module.css';
-import { ReactNode } from 'react';
+import { forwardRef, MutableRefObject, ReactNode } from 'react';
 
 interface UIProductDetailsGalleryProps {
 	children: ReactNode;
+	ref: MutableRefObject<HTMLDivElement | null>;
 }
 
-export const UIProductDetailsGallery = ({ children }: UIProductDetailsGalleryProps) => {
+export const UIProductDetailsGallery = forwardRef<
+	HTMLDivElement,
+	UIProductDetailsGalleryProps
+>(({ children }, ref) => {
 	return (
 		<div
+			ref={ref}
 			className={clsx(
 				classes.gallery,
-				'grid md:grid-cols-[100px_500px] md:grid-rows-[100px_100px_100px_100px] gap-5 grid-rows-[350px_65px] grid-cols-[repeat(4,65px)] md:ml-8'
+
+				'h-[440px] w-[150px] overflow-y-scroll snap-y flex flex-col items-center gap-4 py-2 no-scrollbar'
 			)}
 		>
 			{children}
 		</div>
 	);
-};
+});
