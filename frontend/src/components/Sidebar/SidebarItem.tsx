3db894445d930d53a80ec75c5e005891389eba42
RobunGid <robungidorig@gmail.com>
2025-06-16 22:15:20 +0300

    ➕ Add All Products button to sidebar

--- frontend/src/components/Sidebar/SidebarItem.tsx
+++ frontend/src/components/Sidebar/SidebarItem.tsx
@@ -2,12 +2,19 @@ import { GoHome } from 'react-icons/go';
 import { SidebarItemContainer } from './SidebarItemContainer';
 import { CiGift } from 'react-icons/ci';
 import { MdStarOutline } from 'react-icons/md';
+import { FaBoxes } from 'react-icons/fa';
 
 export const SidebarItem = {
 	Home: () => (
 		<SidebarItemContainer to='/home'>
 			<GoHome />
 			<span className='ms-3'>Home</span>
+		</SidebarItemContainer>
+	),
+	AllProducts: () => (
+		<SidebarItemContainer to='/products'>
+			<FaBoxes />
+			<span className='ms-3'>All Products</span>
 		</SidebarItemContainer>
 	),
 	SecretBoxes: () => (
