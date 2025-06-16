e99df9e5435c1b557bbd670d1aef9f74aa145162
RobunGid <robungidorig@gmail.com>
2025-06-16 22:05:50 +0300

    🛒 UICartQuantityChooser, Cart fixes

--- frontend/src/components/CartModal/Cart.tsx
+++ frontend/src/components/CartModal/Cart.tsx
@@ -7,22 +7,23 @@ export const Cart = () => {
 	const cartItems = useStateSelector((state) => state.cart.productList);
 
 	return (
-		<div className='w-[20rem] md:w-[40rem] h-[40rem] overflow-auto'>
+		<>
 			<h1 className='text-center text-xl font-semibold'>Cart</h1>
+			<div className='w-[20rem] md:w-[40rem] h-[40rem] overflow-auto mt-2'>
+				<div className='grid md:grid-cols-[3fr_2fr_1fr] gap-5 min-w-0 grid-cols-3'>
+					{cartItems.length != 0 && <UICartHeader />}
 
-			<div className='grid md:grid-cols-[3fr_2fr_1fr] gap-5 min-w-0 grid-cols-3'>
-				{cartItems.length != 0 && <UICartHeader />}
-
-				{cartItems.map((cartItem) => (
-					<CartItem cartItem={cartItem} key={cartItem.item.id} />
-				))}
-				{!cartItems.length && (
-					<div className='col-span-3 flex flex-col items-center mt-48'>
-						<div className='text-2xl text-center'>Your cart is empty</div>
-						<CartToProductsButton>Go shopping</CartToProductsButton>
-					</div>
-				)}
+					{cartItems.map((cartItem) => (
+						<CartItem cartItem={cartItem} key={cartItem.item.id} />
+					))}
+					{!cartItems.length && (
+						<div className='col-span-3 flex flex-col items-center mt-48'>
+							<div className='text-2xl text-center'>Your cart is empty</div>
+							<CartToProductsButton>Go shopping</CartToProductsButton>
+						</div>
+					)}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
