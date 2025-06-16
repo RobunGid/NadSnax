98f46147d6f0dd03f26f58f6845d6a6998a2a212
RobunGid <robungidorig@gmail.com>
2025-06-16 23:00:32 +0300

    🗑️ Delete account button fixes

--- frontend/src/components/Account/Settings/UI/UIAccountSettings.tsx
+++ frontend/src/components/Account/Settings/UI/UIAccountSettings.tsx
@@ -5,5 +5,5 @@ interface UIAccountSettingsProps {
 }
 
 export const UIAccountSettings = ({ children }: UIAccountSettingsProps) => {
-	return <div className='flex gap-4 relative flex-wrap'>{children}</div>;
+	return <div className='flex gap-4 relative flex-wrap flex-row'>{children}</div>;
 };
