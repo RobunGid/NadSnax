4f9a821ec42763e183fab326a9e6b1cb5991bdb3
RobunGid <robungidorig@gmail.com>
2025-06-16 21:37:03 +0300

    💸 UIProductDetailsPrice fixes

--- frontend/src/components/ProductDetails/UI/UIProductDetailsPrice.tsx
+++ frontend/src/components/ProductDetails/UI/UIProductDetailsPrice.tsx
@@ -6,12 +6,14 @@ export const UIProductDetailsPrice = ({
 	price,
 	oldPrice,
 }: UIProductDetailsPriceProps) => {
-	return oldPrice ? (
+	return oldPrice && oldPrice != price ? (
 		<div className='flex gap-2 m-4 items-center'>
 			<div className='font-bold text-xl text-lime-600'>Now {price}</div>
 			<div className='font-bold text-md text-gray-500 line-through'>{oldPrice}</div>
 		</div>
 	) : (
-		<div className='font-bold text-xl -mb-4 mt-4 -md:mb-8'>{price}</div>
+		<div className='flex gap-2 m-4 items-center'>
+			<div className='font-bold text-xl'>{price}</div>
+		</div>
 	);
 };
