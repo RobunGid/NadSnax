f38d2660653ad25abc9c23f3889a134f5abbeb2f
RobunGid <robungidorig@gmail.com>
2025-06-16 23:09:54 +0300

    ♻️ CartEmptyMessage added

--- frontend/src/components/CartModal/Cart.tsx
+++ frontend/src/components/CartModal/Cart.tsx
@@ -1,29 +1,24 @@
 import { CartItem } from './CartItem';
 import { useStateSelector } from '../../store';
 import { UICartHeader } from './UI/UICartHeader';
-import { CartToProductsButton } from './CartToProductsButton';
+import { UICart } from './UI/UICart';
+import { UICartTitle } from './UI/UICartTitle';
+import { CartEmptyMessage } from './CartEmptyMessage';
 
 export const Cart = () => {
 	const cartItems = useStateSelector((state) => state.cart.productList);
 
 	return (
 		<>
-			<h1 className='text-center text-xl font-semibold'>Cart</h1>
-			<div className='w-[20rem] md:w-[40rem] h-[40rem] overflow-auto mt-2'>
-				<div className='grid md:grid-cols-[3fr_2fr_1fr] gap-5 min-w-0 grid-cols-3'>
-					{cartItems.length != 0 && <UICartHeader />}
+			<UICartTitle />
+			<UICart>
+				{cartItems.length != 0 && <UICartHeader />}
 
-					{cartItems.map((cartItem) => (
-						<CartItem cartItem={cartItem} key={cartItem.item.id} />
-					))}
-					{!cartItems.length && (
-						<div className='col-span-3 flex flex-col items-center mt-48'>
-							<div className='text-2xl text-center'>Your cart is empty</div>
-							<CartToProductsButton>Go shopping</CartToProductsButton>
-						</div>
-					)}
-				</div>
-			</div>
+				{cartItems.map((cartItem) => (
+					<CartItem cartItem={cartItem} key={cartItem.item.id} />
+				))}
+				{!cartItems.length && <CartEmptyMessage />}
+			</UICart>
 		</>
 	);
 };
