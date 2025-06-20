4771db24f15cbe2d255c0eae0786fb8bde598de4
RobunGid <robungidorig@gmail.com>
2025-06-20 21:45:11 +0300

    💾 Place, order status added to backend

--- backend/models/order.py
+++ backend/models/order.py
@@ -3,11 +3,26 @@ from sqlalchemy import CheckConstraint
 from sqlalchemy.orm import validates
 from datetime import datetime
 from sqlalchemy import DateTime
+import enum
+
+class OrderStatus(enum.Enum):
+    processing = "processing"
+    packing = "packing"
+    shipping = "shipping"
+    ready = "ready"
+    success = "success"
+    canceled = "canceled"
+    returned = "returned"
+    
+    deleted = "deleted"
 
 class OrderModel(db.Model):
     __tablename__ = 'orders'
     
-    id = db.Column(db.String(80), primary_key = True)
+    id = db.Column(db.String(80), primary_key=True)
+    
+    place = db.Column(db.String(255), nullable=False)
+    status = db.Column(db.Enum(OrderStatus), default=OrderStatus.processing)
     
     user_id = db.Column(db.String(80), db.ForeignKey("users.id"), nullable=False)
     user = db.relationship("UserModel", back_populates="orders")
