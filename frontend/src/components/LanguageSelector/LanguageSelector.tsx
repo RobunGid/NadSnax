0b1c1f887f47c1ee20c82d24cc77355401a4a330
RobunGid <robungidorig@gmail.com>
2025-07-01 23:44:45 +0300

    🔧 LanguageSelector shadow added

--- frontend/src/components/LanguageSelector/LanguageSelector.tsx
+++ frontend/src/components/LanguageSelector/LanguageSelector.tsx
@@ -30,7 +30,7 @@ export const LanguageSelector = () => {
 						<button
 							onClick={() => setIsOpen(!isOpen)}
 							className={`inline-flex items-center justify-center w-full rounded-md border border-gray-300 dark:border-gray-600 
-								shadow-sm px-4 py-2 bg-white dark:bg-gray-500 text-sm font-medium text-gray-700 dark:text-gray-200 
+								shadow-xl px-4 py-2 bg-white dark:bg-gray-500 text-sm font-medium text-gray-700 dark:text-gray-200 
 								hover:bg-gray-50 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 ring-gray-700`}
 						>
 							<UIFlagIcon countryCode={selectedLanguage.key} />
@@ -62,7 +62,7 @@ export const LanguageSelector = () => {
 												selectedLanguage.key === language.key
 													? 'bg-gray-100 text-gray-900 dark:bg-gray-400 dark:text-gray-100'
 													: 'text-gray-700 dark:text-gray-100',
-												'px-4 py-2 text-sm text-left items-center hover:bg-gray-100 dark:hover:bg-gray-400 flex ',
+												'px-4 py-2 text-sm text-left items-center hover:bg-gray-100 dark:hover:bg-gray-400 flex shadow-md',
 												index % 2 === 0
 													? 'rounded-r'
 													: 'rounded-l'
