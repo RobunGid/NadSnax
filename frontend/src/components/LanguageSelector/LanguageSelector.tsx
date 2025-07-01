d40a9220731d282b193f927421971537152405e3
RobunGid <robungidorig@gmail.com>
2025-07-01 23:50:36 +0300

    🔧 LanguagePicker style fixes

--- frontend/src/components/LanguageSelector/LanguageSelector.tsx
+++ frontend/src/components/LanguageSelector/LanguageSelector.tsx
@@ -3,6 +3,7 @@ import { useI18n } from '../../i18n/i18n';
 import { Language } from '../../types';
 import { UIFlagIcon } from '../UI/UIFlagIcon';
 import clsx from 'clsx';
+import { IoIosArrowDown } from 'react-icons/io';
 
 export const LanguageSelector = () => {
 	const { lang, setLang } = useI18n();
@@ -35,24 +36,12 @@ export const LanguageSelector = () => {
 						>
 							<UIFlagIcon countryCode={selectedLanguage.key} />
 							{selectedLanguage.name}
-							<svg
-								className='-mr-1 ml-2 h-5 w-5'
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 20 20'
-								fill='currentColor'
-								aria-hidden='true'
-							>
-								<path
-									fillRule='evenodd'
-									d='M10.293 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L10 12.586l3.293-3.293a1 1 0 011.414 1.414l-4 4z'
-									clipRule='evenodd'
-								/>
-							</svg>
+							<IoIosArrowDown size='16' />
 						</button>
 					</div>
 					{isOpen && (
-						<div className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white dark:bg-gray-500 ring-1 ring-black ring-opacity-5'>
-							<div className='grid grid-cols-1 gap-2 p-1' role='none'>
+						<div className='origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white dark:bg-gray-500 ring-1 ring-black ring-opacity-5'>
+							<div className='grid grid-cols-1 gap-2 p-1'>
 								{languages.map((language, index) => {
 									return (
 										<button
@@ -67,7 +56,6 @@ export const LanguageSelector = () => {
 													? 'rounded-r'
 													: 'rounded-l'
 											)}
-											role='menuitem'
 										>
 											<UIFlagIcon countryCode={language.key} />
 											<span className='truncate'>
