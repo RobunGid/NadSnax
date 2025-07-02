14ccc555540394de0427a486599e096a76a3f5c9
RobunGid <robungidorig@gmail.com>
2025-07-02 22:40:54 +0300

    🗝️ Fix keyset resource in backend

--- backend/resources/keysets.py
+++ backend/resources/keysets.py
@@ -9,7 +9,7 @@ blp = Blueprint("keyset", __name__, "Key sets")
 class Keysets(MethodView):
     def get(self, language):
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        keyset_path = os.path.join(current_dir, '..', 'keysets', 'ru.json')
+        keyset_path = os.path.join(current_dir, '..', 'keysets', f'{language}.json')
         with open(keyset_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
             return data
