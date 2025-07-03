18b19d66d5dde82da34f878ae9d1e3632629cfb1
RobunGid <robungidorig@gmail.com>
2025-07-04 00:10:22 +0300

    🔧 UIProductItemBadges position fixes

--- frontend/src/components/ProductItem/UI/UIProductItemBadges.tsx
+++ frontend/src/components/ProductItem/UI/UIProductItemBadges.tsx
@@ -12,12 +12,12 @@ export const UIProductItemBadges = withTranslate(
 		return (
 			<>
 				{isSecretbox && (
-					<div className='absolute bg-purple-300/70 px-2 text-purple-900 font-bold w-52 text-center rotate-[-45deg] top-[47px] left-[-45px]'>
+					<div className='absolute bg-purple-300/70 px-2 text-purple-900 font-bold w-52 text-center rotate-[-45deg] top-[50px] left-[-45px]'>
 						{translate('product_item.badges.secretbox')}
 					</div>
 				)}
 				{isBestseller && (
-					<div className='absolute bg-blue-200/70 px-2 text-blue-900 font-bold w-40 text-center -rotate-45 top-[25px] left-[-45px]'>
+					<div className='absolute bg-blue-200/70 px-2 text-blue-900 font-bold w-40 text-center -rotate-45 top-[27px] left-[-40px]'>
 						{translate('product_item.badges.bestseller')}
 					</div>
 				)}
