0bc97801228a89f9f56ac678c53a714ae6da396f
RobunGid <robungidorig@gmail.com>
2025-07-03 23:17:59 +0300

    🗑️ Delete LanguageSelector from AccountSettings

--- frontend/src/components/Account/Settings/AccountSettings.tsx
+++ frontend/src/components/Account/Settings/AccountSettings.tsx
@@ -2,7 +2,6 @@ import { UIAccountSettings } from './UI/UIAccountSettings';
 import { UIAccountSettingsTitle } from './UI/UIAccountSettingsTitle';
 import { AccountSettingsAvatar } from './AccountSettingsAvatar';
 import { AccountSettingsForm } from './AccountSettingsForm';
-import { AccountSettingsLanguageSelector } from './AccountSettingsLanguageSelector';
 
 export const AccountSettings = () => {
 	return (
@@ -10,7 +9,6 @@ export const AccountSettings = () => {
 			<UIAccountSettingsTitle />
 			<AccountSettingsAvatar />
 			<AccountSettingsForm />
-			<AccountSettingsLanguageSelector />
 		</UIAccountSettings>
 	);
 };
