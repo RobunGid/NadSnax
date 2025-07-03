719960ac305399dd5a47d39832fbb5794d0a0e0b
RobunGid <robungidorig@gmail.com>
2025-07-03 22:47:43 +0300

    🌐 Add translation to cart

--- frontend/src/components/CartModal/CartMessage.tsx
+++ frontend/src/components/CartModal/CartMessage.tsx
@@ -1,3 +1,4 @@
+import { useTranslate } from '../../i18n/i18n';
 import { CartToProductsButton } from './CartToProductsButton';
 
 interface CartMessageProps {
@@ -5,15 +6,16 @@ interface CartMessageProps {
 }
 
 export const CartMessage = ({ type }: CartMessageProps) => {
+	const translate = useTranslate();
 	return (
 		<div className='col-span-3 flex flex-col items-center mt-48'>
 			<div className='text-2xl text-center'>
-				{type == 'empty' && 'Your cart is empty'}
-				{type == 'success' && 'Sucess! Your order is now being process'}
+				{type == 'empty' && translate('cart.message.empty')}
+				{type == 'success' && translate('cart.message.success')}
 			</div>
 			<CartToProductsButton>
-				{type == 'empty' && 'Go shopping'}
-				{type == 'success' && 'Order more'}
+				{type == 'empty' && translate('cart.button.empty')}
+				{type == 'success' && translate('cart.button.success')}
 			</CartToProductsButton>
 		</div>
 	);
