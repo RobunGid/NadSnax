879ae1219c44363d1ab0ac9f010160d7817d7652
RobunGid <robungidorig@gmail.com>
2025-07-04 23:21:15 +0300

    🔧 UIProductDetailsTextArea maxLength add

--- frontend/src/components/ProductDetails/UI/UIProductDetailsTextArea.tsx
+++ frontend/src/components/ProductDetails/UI/UIProductDetailsTextArea.tsx
@@ -4,6 +4,7 @@ export const UIProductDetailsTextArea = () => {
 			className='resize-none shadow-lg outline-none p-4'
 			placeholder='Tell us about your opinion!'
 			name='text'
+			maxLength={160}
 		></textarea>
 	);
 };
