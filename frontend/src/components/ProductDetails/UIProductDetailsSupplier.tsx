6b3819bdc5f18b1370860ab15035415990b633fb
RobunGid <robungidorig@gmail.com>
2025-07-06 00:20:29 +0300

    🈯 Add trnalsation to UIProductDetailsSupplier

--- frontend/src/components/ProductDetails/UIProductDetailsSupplier.tsx
+++ frontend/src/components/ProductDetails/UIProductDetailsSupplier.tsx
@@ -1,19 +1,23 @@
+import { useTranslate } from '../../i18n/i18n';
+import { withTranslate } from '../../logic/withTranslate';
+
 interface UIProductDetailsSupplierProps {
 	supplierLink: string;
+	translate: ReturnType<typeof useTranslate>;
 }
 
-export const UIProductDetailsSupplier = ({
-	supplierLink,
-}: UIProductDetailsSupplierProps) => {
-	return (
-		<div>
-			<a
-				href={supplierLink}
-				target='_blank'
-				className='font-thin underline underline-offset-2'
-			>
-				Visit the supplier site
-			</a>
-		</div>
-	);
-};
+export const UIProductDetailsSupplier = withTranslate(
+	({ supplierLink, translate }: UIProductDetailsSupplierProps) => {
+		return (
+			<div>
+				<a
+					href={supplierLink}
+					target='_blank'
+					className='font-thin underline underline-offset-2'
+				>
+					{translate('product_details.supplier_link')}
+				</a>
+			</div>
+		);
+	}
+);
