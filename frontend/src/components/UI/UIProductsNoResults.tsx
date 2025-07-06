e91a1ec1a66ef5ee876d702a881f4d425b3924a6
RobunGid <robungidorig@gmail.com>
2025-07-06 23:32:42 +0300

    🈯 Add translation to UIProductsNoResults

--- frontend/src/components/UI/UIProductsNoResults.tsx
+++ frontend/src/components/UI/UIProductsNoResults.tsx
@@ -1,41 +1,49 @@
 import clsx from 'clsx';
 import { UIToProductsButton } from './UIToProductsButton';
 import { MouseEventHandler } from 'react';
+import { TaggedText } from '@ayub-begimkulov/i18n';
+import { useTranslate } from '../../i18n/i18n';
+import { withTranslate } from '../../logic/withTranslate';
 
 interface UIProductsNoResultsProps {
 	category?: string;
 	type?: string;
 	className?: string;
 	onClick?: MouseEventHandler<HTMLButtonElement>;
+	translate: ReturnType<typeof useTranslate>;
 }
 
-export const UIProductsNoResults = ({
-	category,
-	type,
-	className,
-	onClick,
-}: UIProductsNoResultsProps) => {
-	return (
-		<div
-			className={clsx(
-				'text-xl flex items-center justify-center flex-col',
-				className
-			)}
-		>
-			{category && type && (
-				<h1>
-					Sorry, there no products for <span className='font-bold'>{type}</span>
-					in <span className='font-bold'>{category}</span>
-				</h1>
-			)}
-			{!type && category && (
-				<h1>
-					Sorry, there no products in{' '}
-					<span className='font-bold'>{category}</span>
-				</h1>
-			)}
-			{!type && !category && <h1>Sorry, there no products</h1>}
-			<UIToProductsButton onClick={onClick}>Open products page</UIToProductsButton>
-		</div>
-	);
-};
+export const UIProductsNoResults = withTranslate(
+	({ category, type, className, onClick, translate }: UIProductsNoResultsProps) => {
+		return (
+			<div
+				className={clsx(
+					'text-xl flex items-center justify-center flex-col',
+					className
+				)}
+			>
+				{category && type && (
+					<h1>
+						<TaggedText
+							text={translate('products.no_results.both')}
+							tags={{
+								1: () => <span className='font-bold'>{type}</span>,
+								2: () => <span className='font-bold'>{category}</span>,
+							}}
+						/>
+					</h1>
+				)}
+				{!type && category && (
+					<h1>
+						Sorry, there no products in
+						<span className='font-bold'>{category}</span>
+					</h1>
+				)}
+				{!type && !category && <h1>Sorry, there no products</h1>}
+				<UIToProductsButton onClick={onClick}>
+					Open products page
+				</UIToProductsButton>
+			</div>
+		);
+	}
+);
