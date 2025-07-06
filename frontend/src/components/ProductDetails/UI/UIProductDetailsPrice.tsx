aa500bba52575c0515479d53d25d6c77e62a64cb
RobunGid <robungidorig@gmail.com>
2025-07-06 23:36:05 +0300

    🈯 UIProductDetailsPrice translation added

--- frontend/src/components/ProductDetails/UI/UIProductDetailsPrice.tsx
+++ frontend/src/components/ProductDetails/UI/UIProductDetailsPrice.tsx
@@ -1,19 +1,26 @@
+import { useTranslate } from '../../../i18n/i18n';
+import { withTranslate } from '../../../logic/withTranslate';
+
 interface UIProductDetailsPriceProps {
 	price: string;
 	oldPrice?: string;
+	translate: ReturnType<typeof useTranslate>;
 }
-export const UIProductDetailsPrice = ({
-	price,
-	oldPrice,
-}: UIProductDetailsPriceProps) => {
-	return oldPrice && oldPrice != price ? (
-		<div className='flex gap-2 m-4 items-center'>
-			<div className='font-bold text-xl text-lime-600'>Now {price}</div>
-			<div className='font-bold text-md text-gray-500 line-through'>{oldPrice}</div>
-		</div>
-	) : (
-		<div className='flex gap-2 m-4 items-center'>
-			<div className='font-bold text-xl'>{price}</div>
-		</div>
-	);
-};
+export const UIProductDetailsPrice = withTranslate(
+	({ price, oldPrice, translate }: UIProductDetailsPriceProps) => {
+		return oldPrice && oldPrice != price ? (
+			<div className='flex gap-2 m-4 items-center'>
+				<div className='font-bold text-xl text-lime-600'>
+					{translate('products_details.price.now')} {price}
+				</div>
+				<div className='font-bold text-md text-gray-500 line-through'>
+					{oldPrice}
+				</div>
+			</div>
+		) : (
+			<div className='flex gap-2 m-4 items-center'>
+				<div className='font-bold text-xl'>{price}</div>
+			</div>
+		);
+	}
+);
