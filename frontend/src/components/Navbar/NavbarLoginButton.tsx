7cc76d5641c2f795533a3db44894266bb73f877a
RobunGid <robungidorig@gmail.com>
2025-07-11 12:03:38 +0300

    🈯 Add translation to login button

--- frontend/src/components/Navbar/NavbarLoginButton.tsx
+++ frontend/src/components/Navbar/NavbarLoginButton.tsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import { LoginModalContext } from '../../context/LoginModalContext';
 import { UINavbarLoginButton } from './UI/UINavbarLoginButton';
+import { useTranslate } from '../../i18n/i18n';
 
 export const NavbarLoginButton = () => {
 	const { toggleLoginModalVisibility } = useContext(LoginModalContext);
+	const translate = useTranslate();
 	return (
 		<UINavbarLoginButton onClick={toggleLoginModalVisibility}>
-			Login
+			{translate('navbar.login_button')}
 		</UINavbarLoginButton>
 	);
 };
