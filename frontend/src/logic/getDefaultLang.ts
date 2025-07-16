829f84d7ffd84ae7e01c66fc8c943be6f3fec48b
RobunGid <robungidorig@gmail.com>
2025-07-16 23:00:57 +0300

    🔧 getDefaultLang fix

--- frontend/src/logic/getDefaultLang.ts
+++ frontend/src/logic/getDefaultLang.ts
@@ -1,10 +1,16 @@
 import { LanguageCodes, predicateLanguageCode } from '../types';
 
 export const getDefaultLang = (): LanguageCodes => {
-	const savedLang = sessionStorage.getItem('lang');
-	if (typeof savedLang == 'string' && predicateLanguageCode(savedLang)) {
-		return savedLang;
-	} else {
-		return 'en';
-	}
+	const sessionStorageLangValue = sessionStorage.getItem('lang');
+
+	const savedLang =
+		typeof sessionStorageLangValue == 'string' &&
+		predicateLanguageCode(sessionStorageLangValue)
+			? sessionStorageLangValue
+			: null;
+	const browserLangValue = navigator.language;
+	const browserLang = predicateLanguageCode(browserLangValue) ? browserLangValue : null;
+	const defaultLang = 'en';
+
+	return savedLang || browserLang || defaultLang;
 };
