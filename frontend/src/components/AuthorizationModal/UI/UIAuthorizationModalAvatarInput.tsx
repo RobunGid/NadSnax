56f97c77d222aba20783f8a0dcbd5deed8043a43
RobunGid <robungidorig@gmail.com>
2025-09-21 20:49:30 +0300

    🔧 Add file name displaying in AvatarFileInput

--- frontend/src/components/AuthorizationModal/UI/UIAuthorizationModalAvatarInput.tsx
+++ frontend/src/components/AuthorizationModal/UI/UIAuthorizationModalAvatarInput.tsx
@@ -2,6 +2,8 @@ import { ChangeEventHandler } from 'react';
 import { useTranslate } from '../../../i18n/i18n';
 import { withTranslate } from '../../../logic/withTranslate';
 import { TaggedText } from '@ayub-begimkulov/i18n';
+import styles from './UIAuthorizationModalAvatarInput.module.css';
+import clsx from 'clsx';
 
 interface UIAuthorizationModalAvatarInputProps {
 	onChange: ChangeEventHandler<HTMLInputElement>;
@@ -14,7 +16,7 @@ export const UIAuthorizationModalAvatarInput = withTranslate(
 			<div className='flex items-center justify-center w-full mb-4'>
 				<label
 					htmlFor='dropzone-file'
-					className='flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500'
+					className='flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500'
 				>
 					<div className='flex flex-col items-center justify-center pt-5 pb-6'>
 						<svg
@@ -48,13 +50,18 @@ export const UIAuthorizationModalAvatarInput = withTranslate(
 						<p className='text-xs text-gray-500 dark:text-gray-400'>
 							{translate('authorization_modal.avatar_input.file_formats')}
 						</p>
+						<div className=' flex justify-center items-center text-center'>
+							<input
+								id='dropzone-file'
+								type='file'
+								className={clsx(
+									styles['avatar-input'],
+									'w-42 text-gray-500 dark:text-gray-400 text-sm'
+								)}
+								onChange={onChange}
+							/>
+						</div>
 					</div>
-					<input
-						id='dropzone-file'
-						type='file'
-						className='hidden'
-						onChange={onChange}
-					/>
 				</label>
 			</div>
 		);
